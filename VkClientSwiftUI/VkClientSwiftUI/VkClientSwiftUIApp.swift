7d58251e7998f1f9324e213fb826191253aa4af2
Constantin <k.kamenchukov@gmail.com>
2022-01-14 15:34:27 +0300

    HW_1

---
+++ VkClientSwiftUI/VkClientSwiftUI/VkClientSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VkClientSwiftUIApp.swift
+//  VkClientSwiftUI
+//
+//  Created by Константин Каменчуков on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct VkClientSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
