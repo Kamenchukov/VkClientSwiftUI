03fb30be48c88b92e6caedeb7c0324ee435b7480
Constantin <k.kamenchukov@gmail.com>
2022-01-18 17:55:29 +0300

    HW_2

---
+++ VkClientSwiftUI/VkClientSwiftUI/GroupCell.swift
@@ -0,0 +1,20 @@
+//
+//  GroupCell.swift
+//  VkClientSwiftUI
+//
+//  Created by Константин Каменчуков on 18.01.2022.
+//
+
+import SwiftUI
+
+struct GroupCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroupCell_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupCell()
+    }
+}
---
+++ VkClientSwiftUI/VkClientSwiftUI/SurNameCell.swift
@@ -0,0 +1,20 @@
+//
+//  SurNameCell.swift
+//  VkClientSwiftUI
+//
+//  Created by Константин Каменчуков on 18.01.2022.
+//
+
+import SwiftUI
+
+struct SurNameCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SurNameCell_Previews: PreviewProvider {
+    static var previews: some View {
+        SurNameCell()
+    }
+}
