42c27db34023b12bd4686488ac84ea032d9d14c5
Constantin <k.kamenchukov@gmail.com>
2022-01-18 17:57:04 +0300

    HW_2 update

--- VkClientSwiftUI/VkClientSwiftUI/GroupCell.swift
+++ VkClientSwiftUI/VkClientSwiftUI/GroupCell.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct GroupCell: View {
+    var groupName: String = "Group name"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+         Text("\(groupName)")
+                .alignmentGuide(HorizontalAlignment.center, computeValue: {$0[.trailing]})
+                Spacer()
+            GroupImage {
+                Image("Matt")
+            }
+            .alignmentGuide(HorizontalAlignment.center, computeValue: {$0[.leading]})
+           Spacer()
+        }
     }
 }
 
-struct GroupCell_Previews: PreviewProvider {
-    static var previews: some View {
-        GroupCell()
+struct GroupImage: View {
+    var groupContent: Image
+    
+    init(@ViewBuilder groupContent: () -> Image ) {
+        self.groupContent = groupContent()
+    }
+    var body: some View {
+        groupContent
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .modifier(CircleShadow(shadowColor: .blue, shadowRadius: 5))
+            .frame(width: 50, height: 50)
     }
 }
+
+//struct GroupCell_Previews: PreviewProvider {
+//    static var previews: some View {
+//        GroupCell()
+//    }
+//}
--- VkClientSwiftUI/VkClientSwiftUI/SurNameCell.swift
+++ VkClientSwiftUI/VkClientSwiftUI/SurNameCell.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct SurNameCell: View {
+    var surName: String = "Surname"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("\(surName)")
+            SurnameImage {
+                Image("unnamed")
+            }
+        }
+    }
+}
+struct CircleShadow: ViewModifier {
+    let shadowColor: Color
+    let shadowRadius: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .background(Circle()
+            .fill(Color.white)
+            .shadow(color: shadowColor, radius: shadowRadius))
     }
 }
 
-struct SurNameCell_Previews: PreviewProvider {
-    static var previews: some View {
-        SurNameCell()
+struct SurnameImage: View {
+    var content: Image
+    
+    init(@ViewBuilder content: () -> Image) {
+        self.content = content()
+    }
+    var body: some View {
+        content
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .modifier(CircleShadow(shadowColor: .black, shadowRadius: 5))
+            .frame(width: 50, height: 50)
+            
     }
 }
+//struct SurNameCell_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SurNameCell()
+//    }
+//}
